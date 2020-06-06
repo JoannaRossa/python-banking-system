ddf4d5a9355dabad523b1d763064aa4e5d06da6c
JoannaRossa <asia.rossa@hotmail.com>
2020-06-05 20:14:57 -0400

    added create and insert examples

---
+++ main.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+
+#Connecting to sqlite
+conn = sqlite3.connect('bank.db')
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+
+#Doping EMPLOYEE table if already exists.
+cursor.execute("DROP TABLE IF EXISTS USER")
+
+#Creating table as per requirement
+sql ='''CREATE TABLE USER(
+   FIRST_NAME CHAR(20) NOT NULL,
+   LAST_NAME CHAR(20),
+   AGE INT,
+   SEX CHAR(1)
+)'''
+
+
+sql2 = '''INSERT INTO USER(
+   FIRST_NAME, LAST_NAME, AGE, SEX) VALUES 
+   ('Joanna', 'Rossa', 27, 'F')'''
+
+sql3 = 
+# sql ='''CREATE TABLE USER(
+#    FIRST_NAME CHAR(20) NOT NULL,
+#    LAST_NAME CHAR(20),
+#    AGE INT,
+#    SEX CHAR(1)
+# )'''
+
+cursor.execute(sql)
+
+
+cursor.execute(sql2)
+
+
+
+# Commit your changes in the database
+conn.commit()
