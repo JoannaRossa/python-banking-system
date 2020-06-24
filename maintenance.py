f0892ff655794114888796ba051a25ade7df4933
JoannaRossa <asia.rossa@hotmail.com>
2020-06-24 11:53:47 -0400

    menu options added

---
+++ maintenance.py
@@ -0,0 +1,4 @@
+class Maintenance:
+    # can turn on/off the systems execution trace
+    # An execution trace is a log of the system’s functions entered, along with the time entered, during the use of the system by the user. In order to associate a particular trace with the user that generated it, the user is identified at the head of the execution trace.
+    # When the trace switch is set to ON, an execution trace is written, for a given account, in an external file that can be printed out at a later time by the maintenance person. No traces are generated when the switch is in the OFF state.
--- system.py
+++ system.py
@@ -22,12 +22,49 @@ class System:
             #if id starts with 0 it is the manager-open manager class
             if first_char == "1":
                 print('Welcome to the manager account')
-                
+                print("Please select from the following options: 1 - open client account, 2 - display client info, 3 - display database info, 4 - close client account")
+                option=int(input("Enter option: "))
+                while option > 0 and option < 5:
+                    if option == 1:
+                        print("Open client account")
+                        break
+                    elif option == 2:
+                        print("Display client info")
+                        break
+                    elif option == 3:
+                        print("Display database info")
+                        break
+                    elif option == 4:
+                        print("Close client account")  
+                        break   
+                    else:
+                        option = input("Error - enter option number from 1 to 4: ")                   
                 break
+    
             #if id starts with 1 it is the client-open client class
             elif first_char == "2":
                 print('Welcome to the client account')
-
+                print("Please select from the following options: 1 - account details, 2 - deposit, 3 - withdraw, 4 - transfer, 5 - contact manager")
+                option=int(input("Enter option: "))
+                while option > 0 and option < 5:
+                    if option == 1:
+                        print("account details")
+                        break
+                    elif option == 2:
+                        print("deposit")
+                        break
+                    elif option == 3:
+                        print("withdraw")
+                        break
+                    elif option == 4:
+                        print("transfer")  
+                        break 
+                    elif option == 5:  
+                        print("contact manager")
+                    
+                    else:
+                        option = input("Error - enter option number from 1 to 4: ")
+                                   
                 break
             #if id starts with 2 it is the maintenance-open maintenance class
             elif first_char == "3":
@@ -49,8 +86,9 @@ def main():
     userPIN = userID.getPIN()
     print(userID.assignRole(userPIN))
 
+
     
-    print(System().assignRole(System().getPIN())
+    # print(System().assignRole(System().getPIN()))
     # userID2 = Account()
 
     # print(s1.getId())
